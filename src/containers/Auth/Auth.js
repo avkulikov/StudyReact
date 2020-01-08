af3d401b8461b2bb6b2232b2aee1b4c0693c990c
Artem Kulikov <a.v.kulikov@novotelecom.ru>
2020-01-09 02:24:58 +0700

    Study React Form. Create Auth Component.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,41 @@
 import React, {Component} from 'react'
+import {Button} from '../../components/Ui/Button/Button'
+import classes from './Auth.module.css'
 
 export default class Auth extends Component {
+    loginHandler = () => {
+
+    }
+    registerHandler = () => {
+
+    }
+
+    submitHandler = event => event.preventDefault()
+
+
+
     render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type="text" />
+                        <input type="text" />
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >
+                            Войти
+                        </Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
