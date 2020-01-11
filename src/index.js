cf759ae68a5d260f5d327c6ec7520908776ddf7d
Artem Kulikov <a.v.kulikov@novotelecom.ru>
2020-01-11 21:41:50 +0700

    Start Study redux,thunk. Добавил в проект redux,thunk. Подключил расширение.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './store/reducers/rootReducer';
+
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+
+const composeEnhancers =
+    typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
+
+
+
+
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
